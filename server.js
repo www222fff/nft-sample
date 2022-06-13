733b128b06041af9c5f287230ba897bde590bd1f
dannyaw <www222fff@163.com>
2022-06-13 14:34:38 +0800

    Removed cors

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const Koa = require('koa');
-const cors = require('@koa/cors');
 const serve = require('koa-static');
 const router = require('./router.js');
 
 const app = new Koa();
 
 app
-  .use(cors())
   .use(serve('./images'))
   .use(router.routes());
 
